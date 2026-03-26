7d8a9d614a4de6cd041837de426b1e2bb3f40379
Bruno Torres <br.torres.del@gmail.com>
2026-03-26 18:00:09 -0300

    HTML da sessão de filtros de pesquisa

--- src/components/HomeFilters/index.jsx
+++ src/components/HomeFilters/index.jsx
@@ -1,10 +1,23 @@
 import './homefilters.styles.css';
 
 export function HomeFilters ({nameFilter, setNameFilter, classFilter, setClassFilter}) {
+
+    const champClasses = ['tank', 'support', 'fighter', 'mage', 'assassin', 'marksman'];
+
     return (
         <div className="home-filters-div">
             <section className="home-filters">
-                
+                <input type="text" name="nameSearch" className='input-name' placeholder='Encontre seu campeão...' />
+                <div className="class-filter-div">
+                    <label htmlFor="class-filter">Classe:</label>
+                    <ul>
+                        {
+                            champClasses.map(c => {
+                                return <li>{c}</li>
+                            })
+                        }
+                    </ul>
+                </div>
             </section>
         </div>
     )
