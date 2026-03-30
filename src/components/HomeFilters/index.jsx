6af1b13aca5be2dfc1600f3f2e3eae74c395704a
Bruno Torres <br.torres.del@gmail.com>
2026-03-30 17:23:22 -0300

    Sessão de filtros finalizada

--- src/components/HomeFilters/index.jsx
+++ src/components/HomeFilters/index.jsx
@@ -8,10 +8,21 @@ export function HomeFilters ({nameFilter, setNameFilter, classFilter, setClassFi
     return (
         <div className="home-filters-div">
             <div className="home-filters">
-                <input type="text" name="nameSearch" className='input-name' placeholder='Encontre seu campeão...' />
+                <input 
+                type="text" 
+                name="nameSearch" 
+                className='input-name' 
+                placeholder='Encontre seu campeão...' 
+                value={nameFilter}
+                onChange={(e) => setNameFilter(e.target.value)}
+                />
                 <div className="class-filter-div">
                     <h4>Classes:</h4>
-                    <ClassesList champClasses={champClasses} />
+                    <ClassesList 
+                    champClasses={champClasses} 
+                    classFilter={classFilter}
+                    setClassFilter={setClassFilter}
+                    />
                 </div>
             </div>
         </div>
