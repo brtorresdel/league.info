46590276b27726a7eed25d25cbf7ae2f1c6f8c7a
Bruno Torres <br.torres.del@gmail.com>
2026-04-06 15:09:42 -0300

    Estilo do botão de menu (mobile) e do nav (desktop)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './header.styles.css';
+import { TiThMenu } from 'react-icons/ti';
+
 
 export function Header() {
     return (
         <header className="header">
             <div className="menu">
-                <button>Home</button>
+                <div className="menu-mobile">
+                    <button><TiThMenu className='icon'/></button>
+                </div>
+                <nav className="menu-desktop">
+                    <button>Início</button>
+                    <button>Sobre o dev</button>
+                </nav>
             </div>
             <div className="title">
                 <h1>leagueinfo.gg</h1>
