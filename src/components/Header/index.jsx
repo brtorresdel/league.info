56cf38803aa7424e4586121e06602466839fbb69
Bruno Torres <br.torres.del@gmail.com>
2026-04-06 14:31:19 -0300

    Ajuste da fonte do título do site

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,17 @@
+import './header.styles.css';
+
 export function Header() {
     return (
-        <header>
-            <h1>League.info</h1>
+        <header className="header">
+            <div className="menu">
+                <button>Home</button>
+            </div>
+            <div className="title">
+                <h1>leagueinfo.gg</h1>
+            </div>
+            <div className="languages">
+                <button>EN</button>
+            </div>
         </header>
     )
 }
