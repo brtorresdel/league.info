f2ce2b252235544b6d9d944effb6a7df27dbd828
Bruno Torres <br.torres.del@gmail.com>
2026-04-09 23:32:33 -0300

    Correção do caminho do gif da katarina dançando, no componente de loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,9 +1,10 @@
 import "./loading.styles.css";
+import dancingKatarinaGif from "../../assets/img/katarina-dancing-gif.gif"
 
 export function Loading () {
     return (
         <div className="loading">
-            <img src="./src/assets/img/katarina-dancing-gif.gif" alt="Imagem da Katarina dançando" />
+            <img src={dancingKatarinaGif} alt="Imagem da Katarina dançando" />
         </div>
     )
 }
